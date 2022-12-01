dd57094738f8008b93c3cdf51956d7d34c4a41e8
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-12-01 20:17:39 +0000

    Refortmat app.py and adjust app layout

--- app.py
+++ app.py
@@ -1,31 +1,34 @@
-#Import the required library
+# Import the required library
 from tkinter import *
 
-#Create an instance of tkinter frame
-win= Tk()
+# Create an instance of tkinter frame
+Root = Tk()
 
-#Define the geometry of the window
-win.geometry("650x450")
+# Define the geometry of the window
+Root.geometry("650x450")
 
-#Define function to hide the widget
+
+# Define function to hide the widget
 def hide_widget(widget):
-   widget.pack_forget()
+    widget.pack_forget()
 
-#Define a function to show the widget
+
+# Define a function to show the widget
 def show_widget(widget):
-   widget.pack()
+    widget.pack()
 
 
-#Create a button Widget
-button_hide= Button(win, text= "Hide", command= lambda:hide_widget(label))
+# Create a button Widget
+button_hide = Button(Root, text="Hide", command=lambda: hide_widget(second_frame))
 button_hide.pack(pady=20)
 
-button_show= Button(win, text= "Show", command= lambda:show_widget(label))
+button_show = Button(Root, text="Show", command=lambda: show_widget(second_frame))
 button_show.pack(pady=20)
 
-#Create an Label Widget
-label= Label(win, text= "Showing the Message", font= ('Helvetica bold', 14))
+second_frame = Frame(Root)
+
+# Create a Label Widget
+label = Label(second_frame, text="Showing the Message", font=('Helvetica bold', 14))
 label.pack(pady=20)
 
-
-win.mainloop()
+Root.mainloop()
